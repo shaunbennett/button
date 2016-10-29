3d0a2827a5dd5c7fa25128238ce6e6d6e4096a5a
Shaun Bennett <me@phinary.ca>
2016-10-29 16:11:32 -0400

    metadata

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,36 @@ var express = require('express');
 var router = express.Router();
 
 var finishTime;
+var cache = {};
+var clickReset = 10 * 1000;
 reset();
 
 /* GET home page. */
 router.get('/button', function(req, res, next) {
-  res.json({ finishTime: finishTime });
+  var uwId = req.params['uwId'];
+  var obj = { finishTime: finishTime };
+  if (uwId && cache[uwId]) {
+    obj.secondsClicked = cache[uwId].secondsClicked;
+    var diff = (new Date.now().getTime()) - cache[uwId].lastClick.getTime();
+    obj.canClick = diff >= clickReset;
+  }
+  res.json(obj);
 });
 
 router.get('/reset', function(req, res, next) {
   reset();
+  res.json({ finishTime: finishTime });
+});
+
+router.get('/click', function (req, res, next) {
+  var uwId = req.params.uwId;
+  var seconds = req.params.clickSeconds;
+  cacheData = {
+    lastClick: Date.now(),
+    secondsClicked: seconds
+  };
+  cache[uwId] = cacheData;
+
   res.json({ finishTime: finishTime });
 });
 
