e58538857422634b8ede147f32c9c54481cc2246
Shaun Bennett <me@phinary.ca>
2016-10-29 12:54:59 -0400

    buttttton

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-var timeLeft = 
+var finishTime;
+reset();
+
 /* GET home page. */
 router.get('/button', function(req, res, next) {
-  res.json({ timeLeft: 1000 * 60 });
+  res.json({ finishTime: finishTime });
 });
+
+router.get('/reset', function(req, res, next) {
+  reset();
+  res.json({ finishTime: finishTime });
+});
+
+function reset() {
+  finishTime = new Date();
+  finishTime.setMilliseconds(finishTime.getMilliseconds() + (1000 * 60 * 60));
+}
 
 module.exports = router;
